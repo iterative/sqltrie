5afd45e2ea13c0f347db103cba332858e89df6e8
Ruslan Kuprieiev <kupruser@gmail.com>
2023-01-16 02:47:45 +0200

    pyinstaller: add package hooks

---
+++ src/sqltrie/__pyinstaller/hook-sqltrie.py
@@ -0,0 +1,6 @@
+# pylint: disable=invalid-name
+from PyInstaller.utils.hooks import (  # pylint: disable=import-error
+    collect_data_files,
+)
+
+datas = collect_data_files("sqltrie", includes=["*.sql"])
